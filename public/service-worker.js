ef7d43ac965dcdedd45ede0f137746027edd3d14
Benjamin Shostak <benshostak@rocketmail.com>
2021-01-31 23:44:49 -0800

    changed manifest data

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,10 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/assets/css/style.css",
-    "/assets/js/loadPosts.js",
-    "/assets/images/Angular-icon.png",
-    "/assets/images/React-icon.png",
-    "/assets/images/Vue.js-icon.png",
+    "/public/style.css",p,
     "/manifest.webmanifest",
-    "/favicon.ico",
-    "/assets/images/icons/icon-72x72.png",
-    "/assets/images/icons/icon-96x96.png",
-    "/assets/images/icons/icon-128x128.png",
-    "/assets/images/icons/icon-144x144.png",
-    "/assets/images/icons/icon-152x152.png",
+    "/public/icons",
     "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-384x384.png",
     "/assets/images/icons/icon-512x512.png",
   ];
   
@@ -29,10 +19,10 @@ const FILES_TO_CACHE = [
         return cache.addAll(FILES_TO_CACHE);
       })
     );
-  
+  //waiting.noWaiting
     self.skipWaiting();
   });
-  
+  //activation of service worker
   self.addEventListener("activate", function(evt) {
     evt.waitUntil(
       caches.keys().then(keyList => {
@@ -53,7 +43,7 @@ const FILES_TO_CACHE = [
   // fetch
   self.addEventListener("fetch", function(evt) {
     // cache successful requests to the API
-    if (evt.request.url.includes("/api/")) {
+    if (evt.request.url.includes("routes/api.js/")) {
       evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
           return fetch(evt.request)
